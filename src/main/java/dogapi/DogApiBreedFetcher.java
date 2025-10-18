b6a7b2b2e3a7a9a116739cabf3e3293fa28c2bf2
janiceelamm <janicelam0811@gmail.com>
2025-10-18 19:06:29 -0400

    worked on task1

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -14,6 +14,9 @@ import java.util.*;
  * Note that all failures get reported as BreedNotFoundException
  * exceptions to align with the requirements of the BreedFetcher interface.
  */
+
+
+
 public class DogApiBreedFetcher implements BreedFetcher {
     private final OkHttpClient client = new OkHttpClient();
 
@@ -23,6 +26,11 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @return list of sub breeds for the given breed
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
+
+// this method is for running the url
+// put the run method under this class
+
+
     @Override
     public List<String> getSubBreeds(String breed) {
         // TODO Task 1: Complete this method based on its provided documentation
@@ -30,6 +38,41 @@ public class DogApiBreedFetcher implements BreedFetcher {
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+
+        String url = "https://dog.ceo/api/breeds/list/all";
+        String jsonResponse = null;
+        try {
+            jsonResponse = run(url);
+        } catch (IOException e) {
+//            throw new RuntimeException(e); //i dont know why we are doing this
+        }
+
+        //parse json response; two nested arrays
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+        JSONObject messageObject = jsonObject.getJSONObject("message");
+
+        if (messageObject.has(breed)){
+            JSONArray subbreedArray = messageObject.getJSONArray(breed);
+
+            List<String> subbreed = new ArrayList<>();
+            for (int i = 0; i < subbreedArray.length();i++){
+                subbreed.add(subbreedArray.getString(i));
+            }
+
+            return subbreed;
+
+        }
+        else {
+            throw new BreedNotFoundException("Breed Not Found");
+        }
+
+    }
+
+    public String run (String url) throws IOException {
+        Request request = new Request.Builder().url(url).build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
 }
